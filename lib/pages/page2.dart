2648d452120ea1d0a604fa3dd63b5efcd1134efe
ymt117 <yamamto117@gmail.com>
2019-05-11 23:08:04 +0900

    Add DatePicker

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -11,14 +11,27 @@ class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-          child: Column(
-        children: <Widget>[
-          timerPicker,
+      body: ListView(
+        children: [
+          FlatButton(
+              onPressed: () {
+                DatePicker.showDatePicker(context,
+                    showTitleActions: true,
+                    minTime: DateTime(2018, 3, 5),
+                    maxTime: DateTime(2019, 6, 7), onChanged: (date) {
+                  print('change $date');
+                }, onConfirm: (date) {
+                  print('change $date');
+                }, currentTime: DateTime.now(), locale: LocaleType.jp);
+              },
+              child: Text(
+                'show date time picker (Japanese)',
+                style: TextStyle(color: Colors.blue),
+              )),
           textBox,
           buttonSection,
         ],
-      )),
+      ),
     );
   }
 
