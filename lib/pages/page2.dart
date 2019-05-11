4263911bdf49ebfe6409b7b4da91479b06ae0a93
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2019-05-11 19:01:04 +0900

    Change Routing

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -1,17 +1,18 @@
 import 'package:flutter/material.dart';
 
-class SecondScreen extends StatelessWidget {
+class SecondScreen extends StatefulWidget{
+  @override
+  _SecondScreenState createState() => new _SecondScreenState();
+  
+}
+
+class _SecondScreenState extends State<SecondScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text("Second Screen"),
-      ),
       body: Center(
         child: RaisedButton(
           onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack
             Navigator.pop(context);
           },
           child: Text('Go back!'),
