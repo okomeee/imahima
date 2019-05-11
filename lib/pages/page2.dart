530565235593c5f0e10b25d27fa3e047611f65f6
ymt117 <yamamto117@gmail.com>
2019-05-11 18:15:43 +0900

    Add textBox and buttonSection

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -9,15 +9,30 @@ class SecondScreen extends StatelessWidget {
         title: Text("Second Screen"),
       ),
       body: Center(
-        child: RaisedButton(
-          onPressed: () {
-            // Navigate back to the first screen by popping the current route
-            // off the stack
-            Navigator.pop(context);
-          },
-          child: Text('Go back!'),
-        ),
-      ),
+          child: Column(
+        children: <Widget>[
+          textBox,
+          buttonSection,
+        ],
+      )),
     );
   }
+
+  Widget textBox = Container(
+    padding: const EdgeInsets.all(32),
+    child: TextField(
+      keyboardType: TextInputType.multiline,
+      maxLines: null,
+      decoration: InputDecoration(hintText: '遊びたい内容を入力してください'),
+    ),
+  );
+
+  Widget buttonSection = Container(
+    child: RaisedButton(
+      onPressed: () {
+        // 入力した「いまヒマ」情報をアップロードする処理
+      },
+      child: Text('いまヒマ'),
+    ),
+  );
 }
