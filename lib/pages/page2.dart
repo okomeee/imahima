6a07a379ea43c0ec4addce2fc0d01b2b14792f91
ymt117 <yamamto117@gmail.com>
2019-05-12 01:15:50 +0900

    Change buttonSection messeage

--- lib/pages/page2.dart
+++ lib/pages/page2.dart
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 // ymt117
 import 'package:flutter/material.dart';
 import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
@@ -54,64 +53,7 @@ class _SecondScreenState extends State<SecondScreen> {
       onPressed: () {
         // 入力した「いまヒマ」情報をアップロードする処理
       },
-      child: Text('いまヒマ'),
+      child: Text('「いまヒマ」情報を登録する'),
     ),
   );
 }
-=======
-// ymt117
-import 'package:flutter/material.dart';
-import 'package:flutter_datetime_picker/flutter_datetime_picker.dart';
-
-class SecondScreen extends StatefulWidget {
-  @override
-  _SecondScreenState createState() => new _SecondScreenState();
-}
-
-class _SecondScreenState extends State<SecondScreen> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: ListView(
-        children: [
-          FlatButton(
-              onPressed: () {
-                DatePicker.showDatePicker(context,
-                    showTitleActions: true,
-                    minTime: DateTime(2018, 3, 5),
-                    maxTime: DateTime(2019, 6, 7), onChanged: (date) {
-                  print('change $date');
-                }, onConfirm: (date) {
-                  print('change $date');
-                }, currentTime: DateTime.now(), locale: LocaleType.en);
-              },
-              child: Text(
-                'show date time picker (English)',
-                style: TextStyle(color: Colors.blue),
-              )),
-          textBox,
-          buttonSection,
-        ],
-      ),
-    );
-  }
-
-  Widget textBox = Container(
-    padding: const EdgeInsets.all(32),
-    child: TextField(
-      keyboardType: TextInputType.multiline,
-      maxLines: null,
-      decoration: InputDecoration(hintText: '遊びたい内容を入力してください'),
-    ),
-  );
-
-  Widget buttonSection = Container(
-    child: RaisedButton(
-      onPressed: () {
-        // 入力した「いまヒマ」情報をアップロードする処理
-      },
-      child: Text('いまヒマ'),
-    ),
-  );
-}
->>>>>>> de62a30c59350570c59e099de6f5e0d5542c0f33
