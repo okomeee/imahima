ff4ae9943e045c262f80ae4c7576195a8ac79289
Ehama Seiya <31039439+hoshinari@users.noreply.github.com>
2019-05-12 10:39:25 +0900

    Add login

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'pages/init.dart';
+import 'pages/login.dart';
 
 void main() {
   runApp(App());
@@ -10,9 +11,10 @@ class App extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: '今暇',
-      initialRoute: '/',//最初のページを指定
+      initialRoute: '/login',//最初のページを指定
       routes: <String, WidgetBuilder>{
         '/': (_) => new InitScreen(),
+        '/login':(_)=> new Login(),
       },
       theme: new ThemeData.dark(),
     );
